7ff73e62b531e7d300f5c4bf7e728453c4618268
Andrew Laffin <andrewlaffin@gmail.com>
2013-01-29 18:27:49 -0500

    Fix build

--- BixiTO/src/com/example/bixito/MainActivity.java
+++ BixiTO/src/com/example/bixito/MainActivity.java
@@ -1,19 +1,16 @@
 package com.example.bixito;
 
 import android.app.ActionBar;
+import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity implements
 		ActionBar.TabListener {
