253ae18b673ad5bf8c7d5ca134bb2b6fce459a04
Andrew Laffin <andrewlaffin@gmail.com>
2013-01-31 02:40:23 -0500

    Add the list view fragment to start displaying some dummy data

---
+++ BixiTO/src/com/example/bixito/ListViewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.bixito;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.bixito.station.BikeStation;
+
+public class ListViewAdapter extends BaseAdapter {
+
+	private Context context;
+	private ArrayList<BikeStation> bikeStations;
+	
+	public ListViewAdapter(Context context, ArrayList<BikeStation> bikeStations){
+		this.context = context;
+		this.bikeStations = bikeStations;
+	}
+	
+	/**
+	 * Returns the number of bike stations.
+	 */
+	public int getCount() {
+		return bikeStations.size();
+	}
+
+	public Object getItem(int position) {
+		return bikeStations.get(position);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		BikeStation station = bikeStations.get(position);
+		
+		//If the view for the item hasn't been created yet, create it
+		if(convertView == null){
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.list_item, null);
+		}
+		
+		//Temporary to display some dummy data
+		TextView text1 = (TextView) convertView.findViewById(R.id.text1);
+		text1.setText(station.getStationName());
+		
+		TextView text2 = (TextView) convertView.findViewById(R.id.text2);
+		text2.setText(station.getTerminalName());
+		
+		
+		return convertView;
+		
+	}
+
+}
--- BixiTO/src/com/example/bixito/ListViewFragment.java
+++ BixiTO/src/com/example/bixito/ListViewFragment.java
@@ -1,7 +1,24 @@
 package com.example.bixito;
 
+import java.util.ArrayList;
+
 import android.app.ListFragment;
+import android.os.Bundle;
+
+import com.example.bixito.station.BikeStation;
+import com.example.bixito.station.StationParser;
 
 public class ListViewFragment extends ListFragment {
 
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		StationParser stationParser = new StationParser("dummy");
+		ArrayList<BikeStation> stationList = stationParser.getDummyStationList();
+		
+		ListViewAdapter adapter = new ListViewAdapter(getActivity(), stationList);
+		setListAdapter(adapter);
+		//TODO: Make use of List_view layout
+		//TODO: Use real data
+	}
+	
 }
